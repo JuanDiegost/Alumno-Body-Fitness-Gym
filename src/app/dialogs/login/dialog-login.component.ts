34d4239c66045e41135b28f732bac2d2a2400a3e
juandiegost <molinajuan149@gmail.com>
2019-02-07 12:35:14 -0500

    .

--- src/app/dialogs/login/dialog-login.component.ts
+++ src/app/dialogs/login/dialog-login.component.ts
@@ -1,26 +1,33 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
-import {HomeComponent} from '../../home/home.component';
-import {PackageRouterService, User, UserType} from '../../interfaces';
+import { Component, Inject, OnInit } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { HomeComponent } from "../../home/home.component";
+import { PackageRouterService, User, UserType } from "../../interfaces";
 
-import {RoutersApp} from '../../util/RoutersApp';
+import { RoutersApp } from "../../util/RoutersApp";
+import { Notifies } from "../../util/Notifies";
 
 @Component({
-  templateUrl: './dialog-login.component.html',
-  styleUrls: ['./dialog-login.component.css']
+  templateUrl: "./dialog-login.component.html",
+  styleUrls: ["./dialog-login.component.css"]
 })
 // @ts-ignore
 export class DialogLoginComponent implements OnInit {
-
   // @ts-ignore
-  constructor(public dialogRef: MatDialogRef<HomeComponent>, @Inject(MAT_DIALOG_DATA) private data: PackageRouterService) {}
+  constructor(
+    public dialogRef: MatDialogRef<HomeComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: PackageRouterService,
+    public notifies: Notifies
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   loginUser(event: Event) {
-    const elementUserName = (<HTMLInputElement>document.getElementById('userName'));
-    const elementPassword = (<HTMLInputElement>document.getElementById('password'));
+    const elementUserName = <HTMLInputElement>(
+      document.getElementById("userName")
+    );
+    const elementPassword = <HTMLInputElement>(
+      document.getElementById("password")
+    );
     const username: string = elementUserName.value;
     const password: string = elementPassword.value;
     event.preventDefault();
@@ -28,20 +35,30 @@ export class DialogLoginComponent implements OnInit {
     this.data.servicePageHome.login(username, password).subscribe(
       res => {
         console.log(res);
-        if (res[0]["dniAlumno"]!=undefined) {
-          const user: User = {name: username, type: UserType.STUDENT,id:res[0]["idAlumno"]};
+        if (res[0]["dniAlumno"] != undefined) {
+          const user: User = {
+            name: username,
+            type: UserType.STUDENT,
+            id: res[0]["idAlumno"]
+          };
           this.data.serviceLogin.setUserLoggedIn(user);
-          localStorage.setItem('idAlumno', res[0]["dniAlumno"]);
+          localStorage.setItem("idAlumno", res[0]["dniAlumno"]);
         } else {
-          alert('Usuario o contraseña incorrecta');
-          elementPassword.value = '';
+          this.notifies.showError(
+            "Error de credenciales",
+            "Usuario o contraseña incorrecta"
+          );
+          elementPassword.value = "";
           return;
         }
       },
       error => {
         console.error(error);
-        alert('No podemos conectarnos con el servidor en este momento, por favor comprueba tu conexion a internet o intenta mas tarde.');
-        elementPassword.value = '';
+        this.notifies.showWarning(
+          "Error de servidor",
+          "No podemos conectarnos con el servidor en este momento, por favor comprueba tu conexion a internet o intenta mas tarde."
+        );
+        elementPassword.value = "";
       },
       () => this.navigate(RoutersApp.student)
     );
